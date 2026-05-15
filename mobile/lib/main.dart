3fb9e2227e3e55d600d8fed1d664ad6fcea20bcb
Jose Miguel <josemiguel@MacBook-Air-de-Jose.local>
2026-05-15 10:07:21 -0400

    sentry

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 import 'core/auth/auth_provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,10 +10,23 @@ void main() async {
   final container = ProviderContainer();
   await container.read(authProvider.notifier).restoreSession();
 
-  runApp(
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://5a74af34355c0a9f999e6c83bbf2f13a@o148559.ingest.us.sentry.io/4511394261172224';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(SentryWidget(child: 
     UncontrolledProviderScope(
       container: container,
       child: const LogiFlowApp(),
     ),
+  )),
   );
+  // TODO: Remove this line after sending the first sample event to sentry.
+  await Sentry.captureException(Exception('This is a sample exception.'));
 }
